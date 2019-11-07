586b6708f39c02b9a3dfddf762a2e0d4162e4ddc
Edith Gozdzik <itsdishx27@gmail.com>
2019-11-07 15:46:16 -0500

    Theme_cc_a11y

---
+++ theme_cc_a11y.php
@@ -0,0 +1,13 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+ 
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+ 
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Theme cc_a11y is a child theme of Boost. It attempts to be optimized for accessibility.';                
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'CC_A11y';                                                                                                    
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
+?>
